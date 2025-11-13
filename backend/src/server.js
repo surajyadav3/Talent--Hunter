42f9350300a8cbf10956401106387ab316163b67
SURAJ YADAV <surajkosliya2004@gmail.com>
2025-11-13 23:19:15 +0530

    deployment conflict edit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,28 @@
 import express from "express";
+import path from "path";
 import {ENV} from "./lib/env.js";
 
 const app = express();
 
-console.log(ENV.PORT);
-console.log(ENV.DB_URL);
+const __dirname=path.resolve()
+
 
 app.get("/health",(req,res) =>{
      res.status(200).json({msg:"API  is up and running"})
-})
+});
+app.get("/books",(req,res) =>{
+     res.status(200).json({msg:"This is the books endpoint"})
+});
+
+//make app our app ready for deployment
+if(ENV.NODE_ENV === "production"){
+     app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+     app.get("/{*any}",(req,res)=>{
+          res.sendFile(path.join(__dirname,"../frontend","dist" , "index.html"));
+     });
+}
+
+
 
 app.listen(ENV.PORT ,() => console.log("Server is running on PORT:" ,ENV.PORT));
