40939f0a692daa308da28bee20b9f8b6a80113cc
SURAJ YADAV <surajkosliya2004@gmail.com>
2025-11-16 22:36:12 +0530

    auth and db changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,10 +3,21 @@ import path from "path";
 import {ENV} from "./lib/env.js";
 import { connect } from "http2";
 import { connectDB } from "./lib/db.js";
+import cors from cors;
+import {serve} from "inngest/express";
 
 const app = express();
 
 const __dirname=path.resolve()
+
+//middlewares
+app.use(express.json());
+//credentials true means server allows the browser to include cookies on request
+
+
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
+
+app.use("/api/inngest",serve({client:inngest,functions}));
 
 
 app.get("/health",(req,res) =>{
