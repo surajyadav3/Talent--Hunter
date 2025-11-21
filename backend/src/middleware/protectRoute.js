2f25c7bd35253435c75c053e9a8d4be1a1a0839f
SURAJ YADAV <surajkosliya2004@gmail.com>
2025-11-21 15:29:33 +0530

    minor  type of fixes

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -6,12 +6,12 @@ export const protectRoute = [
      async(req ,res ,next) =>{
           try{
                const clerkId= req.auth().userId;
-               if(!clerkId) return res.status(401).json({msg:"Unauthorized - invalid token"})
+               if(!clerkId) return res.status(401).json({message:"Unauthorized - invalid token"})
 
                     //find user in db by clerk ID;
                     const user=await User.findOne({clerkId})
 
-                    if(!user) return res.status(404).json({msg:"User not found"})
+                    if(!user) return res.status(404).json({message:"User not found"})
 
                          req.user=user
                          next()
