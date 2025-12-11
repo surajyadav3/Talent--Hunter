a2697227cf7fd64b6dd26e3a8c59e08c37c75d68
SURAJ YADAV <surajkosliya2004@gmail.com>
2025-12-12 04:02:32 +0530

    frontend-tools-setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,27 @@
-import './App.css'
-import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton } from '@clerk/clerk-react'
+import { SignedIn, SignedOut, SignInButton, SignOutButton, UserButton , useUser } from '@clerk/clerk-react'
+import {  Navigate ,Route ,Routes } from 'react-router'
+import HomePage from './pages/HomePage'
+
+import { Toaster } from 'react-hot-toast'
 
 function App() {
+
+  const {isSignedIn}=useUser()
+
   return (
     <>
-      <h1>Welcome to the App</h1>
-      <SignedOut>
-         <SignInButton type="modal">
-          <button className="">Login</button>
-         </SignInButton>
-      </SignedOut>
-
-
-      <SignedIn>
-        <SignOutButton/>
-      </SignedIn>
-
-      <UserButton/>
+    <Routes>
+      <Route path ="/" element = {<HomePage/>} />
     
+      <Route path ="/problems" element = {isSignedIn ? <ProblemsPage />: <Navigate to={"/"}/>} />
+    </Routes>
+
+    <Toaster   toastOptions={{duration:5000}}/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+
+//tw , daisyui , react-router , react-hot-toast , react-query transtack query ,axios
